0b21d79621ef37f56355875b85ecdde4314d07a5
Luis Angel Quiñones Diaz <luanquidi1@hotmail.com>
2019-11-20 14:20:25 -0500

    first commit

---
+++ build/precache-manifest.4bf4c395af6b3ee79fc833713e150dc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ac192f73c1399b2d9a9",
+    "url": "/static/css/main.e6d2f011.chunk.css"
+  },
+  {
+    "revision": "5ac192f73c1399b2d9a9",
+    "url": "/static/js/main.56f708a8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ecb2ae0344a8eaf8a6fe",
+    "url": "/static/js/2.1892b01a.chunk.js"
+  },
+  {
+    "revision": "258d9e6e61751f5a4aef396596c11700",
+    "url": "/index.html"
+  }
+];
